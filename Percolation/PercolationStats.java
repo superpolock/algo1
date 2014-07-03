60f1885df9cb3d59ccbca515aa4b1a6d2c242e46
Dan Pieczynski <danpieczynski@sbcglobal.net>
2014-07-02 19:28:44 -0500

    One more time....

--- Percolation/PercolationStats.java
+++ Percolation/PercolationStats.java
@@ -1,38 +1,62 @@
 public class PercolationStats {
     private int[] results; 
     private int sampleSize;
+    
+    private int totalCellsPopulated = 0;
+    private int totalPercolations = 0;
+    
+    private double m_mean = 0;
+    private double m_stddev = 0;
+    private double m_conLo = 0;
+    private double m_conHi = 0;
+    
     public PercolationStats(int N, int T) {    // perform T independent computational experiments on an N-by-N grid
         results = new int[T];
+        totalCellsPopulated = 0;
+        totalPercolations = 0;
+        int[] availableCells = new int[N*N];
+        for ( int idx = 0; idx < N*N; ++idx ) {
+        	availableCells[idx]=idx;
+        }
         for ( int idx = 0; idx < T; ++idx ) {
             Percolation p = new Percolation(N);
             int cellsFilled=0;
-            int attempts = 0;
+            int cellsRemaining = N * N;
             do {
-            	int i;
-            	int j;
-            	do {
-                	i = StdRandom.uniform(1,N);
-                	j = StdRandom.uniform(1,N);
-                	++attempts;
-            	} while ( p.isOpen(i, j) && attempts < N*N );
+            	int availableIdx = StdRandom.uniform( 0, cellsRemaining );
+            	int cellToPopulate = availableCells[ availableIdx ];
+            	if ( --cellsRemaining != availableIdx ) {
+	            	availableCells[ availableIdx ] = availableCells[ cellsRemaining ];
+	            	availableCells[ cellsRemaining ] = cellToPopulate;
+            	}
+            	int i = cellToPopulate / N + 1;
+            	int j = cellToPopulate % N + 1;
+            	assert( p.isOpen(i, j) );
                 p.open(i,j);
                 ++cellsFilled;
-            } while ( !p.percolates() && attempts < 2*N );
+            } while ( !p.percolates() );
+            ++totalPercolations;
+            totalCellsPopulated += cellsFilled;
             results[idx] = cellsFilled;
         }
         sampleSize = T;
+        
+        m_mean = StdStats.mean(results);
+        m_stddev = StdStats.stddev(results);
+        m_conLo = mean() - (1.96*stddev()/Math.sqrt(sampleSize));
+        m_conHi = mean() + (1.96*stddev()/Math.sqrt(sampleSize));
     }
     public double mean() {                     // sample mean of percolation threshold
-        return StdStats.mean(results);
+        return m_mean;
     }
     public double stddev() {                  // sample standard deviation of percolation threshold
-        return StdStats.stddev(results);
+        return m_stddev;
     }
     public double confidenceLo() {            // returns lower bound of the 95% confidence interval
-        return mean() - (1.96*stddev()/Math.sqrt(sampleSize));
+        return m_conLo;
     }
     public double confidenceHi() {            // returns upper bound of the 95% confidence interval
-        return mean() + (1.96*stddev()/Math.sqrt(sampleSize));
+        return m_conHi;
     }
     public static void main(String[] args) {  // test client, described below
         if ( args.length == 3 ) {
