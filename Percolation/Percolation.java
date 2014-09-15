32351b0d82377463dedc4be4f5a23e323ac5c025
Dan Pieczynski <danpieczynski@sbcglobal.net>
2014-09-15 13:13:22 -0500

    100% Just need to improve style

--- Percolation/Percolation.java
+++ Percolation/Percolation.java
@@ -3,6 +3,7 @@
 public class Percolation {
 	// Stores which cells are joined
 	private WeightedQuickUnionUF uf;
+	private WeightedQuickUnionUF percTester;
 	// Number of rows/cols
 	private int width;
 	// Right bottom most cell, to determine if top percolates to bottom
@@ -18,12 +19,16 @@ public class Percolation {
 		}
 		width = N;
 		uf = new WeightedQuickUnionUF(N * (N + 2));
+		percTester = new WeightedQuickUnionUF(N * (N + 2));
 		bottomCell = N * (N + 2) - 1;
 		openCells = new boolean[bottomCell + 1];
 
 		for (int n = 1; n < width; n++) {
 			// open the fake top row
 			uf.union(0, n);
+			// We have a duplicate UF with a fake bottom row to quickly test if we percolate
+			percTester.union(0,n);
+			percTester.union(bottomCell, bottomCell-n);
 		}
 		for (int n = 0; n < width; n++) {
 			openCells[n] = true;
@@ -55,32 +60,38 @@ public class Percolation {
 			if (i > 1 ) {
 				if (isOpen(i - 1, j)) {
 					uf.union(calcIndex(i - 1, j), calcIndex(i, j));
+					percTester.union(calcIndex(i - 1, j), calcIndex(i, j));
 				}
 			}
 			else if ( i == 1 ) {
 				uf.union(calcIndex(i,j)-width, calcIndex(i,j));
+				percTester.union(calcIndex(i,j)-width, calcIndex(i,j));
 			}
 			if (i < width && isOpen(i + 1, j)) {
 				uf.union(calcIndex(i + 1, j), calcIndex(i, j));
+				percTester.union(calcIndex(i + 1, j), calcIndex(i, j));
 			}
 			else if ( i == width ) {
 				uf.union(calcIndex(i, j)+width, calcIndex(i, j));
+				percTester.union(calcIndex(i, j)+width, calcIndex(i, j));
 			}
 			if (j > 1 ) {
 				if ( isOpen(i, j - 1) ) {
 					uf.union(calcIndex(i, j - 1), calcIndex(i, j));
+					percTester.union(calcIndex(i, j - 1), calcIndex(i, j));
 				}
 			}
 			if (j < width && isOpen(i, j + 1)) {
 				uf.union(calcIndex(i, j + 1), calcIndex(i, j));
+				percTester.union(calcIndex(i, j + 1), calcIndex(i, j));
 			}
 			
-			if ( !m_doesPercolate && uf.connected(0,localIdx) ) {
+/*			if ( !m_doesPercolate && uf.connected(0,localIdx) ) {
 				int n = 0;
 				do {
 					m_doesPercolate = uf.connected( localIdx, this.bottomCell - n );
 				} while ( !m_doesPercolate && ++n < width ); 
-			}
+			}*/
 		}
 	}
 
@@ -98,7 +109,7 @@ public class Percolation {
 
 	// Is there a path of "open" cells from top to bottom?
 	public boolean percolates() { 
-		return m_doesPercolate;
+		return percTester.find(0) == percTester.find(bottomCell);
 	}
 
 	public static void main(final String[] args) { // test client, described
